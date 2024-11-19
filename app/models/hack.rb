dbcd65a4ff5bddb3b1b8be4e23d3c5b9579de6c3
Jorge <jorgeluiscardoza89@gmail.com>
2024-11-19 17:18:35 +0300

    Update hack.rb

--- app/models/hack.rb
+++ app/models/hack.rb
@@ -2,6 +2,4 @@ class Hack < ApplicationRecord
   belongs_to :article
   has_one :source, through: :article
   has_many :hack_categories
-  has_and_belongs_to_many :categories, through: :hack_categories
-  has_and_belongs_to_many :classifications, through: :categories
 end
