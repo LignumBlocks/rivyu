14b9c496fe311e06989c87b437ceb4feb1818b2e
Jorge <jorgeluiscardoza89@gmail.com>
2024-11-27 16:11:20 +0300

    Update 20241127130143_add_necessary_indexes.rb

--- db/migrate/20241127130143_add_necessary_indexes.rb
+++ db/migrate/20241127130143_add_necessary_indexes.rb
@@ -1,6 +1,6 @@
 class AddNecessaryIndexes < ActiveRecord::Migration[7.0]
   def change
-    add_index :hack_categories, %i[hack_id category_id], unique: true
+    add_index :hack_categories, %i[hack_id category_id]
     add_index :categories, %i[classification_id name]
     add_index :hacks, :is_advice
     add_index :hacks, :main_goal
