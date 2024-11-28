6cd5263c5b7aba3bf96edb7e76cef339ebac8c03
Jorge <jorgeluiscardoza89@gmail.com>
2024-11-28 12:27:08 +0300

    Improve filters

--- app/controllers/hacks_controller.rb
+++ app/controllers/hacks_controller.rb
@@ -2,8 +2,10 @@ require 'open-uri'
 
 class HacksController < ApplicationController
   def index
-    @q = Hack.ransack(params[:q])
+    filters = combined_filters
+    @q = Hack.ransack(filters)
     @pagy, @hacks = pagy(@q.result.distinct.order(created_at: :desc), items: 50, size: [1, 3, 3, 1])
+    @q = Hack.ransack(params[:q])
   end
 
   def show
@@ -26,32 +28,23 @@ class HacksController < ApplicationController
 
   private
 
-  def convert_dates_to_yyyymmdd(params, *keys)
-    keys.each do |key|
-      next unless params&.dig(key).present?
+  def combined_filters
+    combined_filter_ids = [
+      params.dig(:q, :with_scale_ids),
+      params.dig(:q, :with_popularity_ids),
+      params.dig(:q, :with_financial_topic_ids),
+      params.dig(:q, :with_audience_ids),
+      params.dig(:q, :with_horizon_ids),
+      params.dig(:q, :with_risk_ids),
+      params.dig(:q, :with_implementation_ids),
+      params.dig(:q, :with_financial_ids),
+      params.dig(:q, :with_knowledge_ids)
+    ].compact.flatten.uniq
 
-      begin
-        params[key] = Date.strptime(params[key], '%m/%d/%Y').strftime('%Y-%m-%d')
-      rescue ArgumentError
-        flash[:alert] = 'Invalid date format'
-      end
-    end
-  end
-
-  def valid_hacks_ransack(query_params)
-    current_user.hacks.joins(:hack_structured_info, :hack_validation)
-                .where(hack_validations: { status: true })
-                .ransack(query_params)
-  end
-
-  def not_valid_hacks_ransack(query_params)
-    current_user.hacks
-                .left_joins(:hack_structured_info, :hack_validation)
-                .where('hack_structured_infos.id IS NULL OR hack_validations.status = false OR hack_validations.status IS NULL')
-                .ransack(query_params)
-  end
-
-  def hack_params
-    params.permit(:q)
+    {
+      is_advice_eq: params[:q][:is_advice_eq],
+      source_id_eq: params[:q][:source_id_eq],
+      hack_categories_category_id_in: combined_filter_ids
+    }
   end
 end
--- app/models/hack.rb
+++ app/models/hack.rb
@@ -3,57 +3,6 @@ class Hack < ApplicationRecord
   has_one :source, through: :article
   has_many :hack_categories
   has_many :categories, through: :hack_categories
-
   has_many :superhack_sources
   has_many :superhacks, through: :superhack_sources
-
-  def self.with_popularity_ids(*ids)
-    popular_category_ids = Category.popularity_categories.where(id: ids).select(:id)
-    joins(:hack_categories).where(hack_categories: { category_id: popular_category_ids }).distinct
-  end
-
-  def self.with_financial_topic_ids(*ids)
-    financial_topic_category_ids = Category.financial_topic_categories.where(id: ids).select(:id)
-    joins(:hack_categories).where(hack_categories: { category_id: financial_topic_category_ids }).distinct
-  end
-
-  def self.with_audience_ids(*ids)
-    audience_category_ids = Category.audience_categories.where(id: ids).select(:id)
-    joins(:hack_categories).where(hack_categories: { category_id: audience_category_ids }).distinct
-  end
-
-  def self.with_horizon_ids(*ids)
-    horizon_category_ids = Category.horizon_categories.where(id: ids).select(:id)
-    joins(:hack_categories).where(hack_categories: { category_id: horizon_category_ids }).distinct
-  end
-
-  def self.with_risk_ids(*ids)
-    risk_category_ids = Category.risk_categories.where(id: ids).select(:id)
-    joins(:hack_categories).where(hack_categories: { category_id: risk_category_ids }).distinct
-  end
-
-  def self.with_implementation_ids(*ids)
-    implementation_category_ids = Category.implementation_categories.where(id: ids).select(:id)
-    joins(:hack_categories).where(hack_categories: { category_id: implementation_category_ids }).distinct
-  end
-
-  def self.with_financial_ids(*ids)
-    financial_category_ids = Category.financial_categories.where(id: ids).select(:id)
-    joins(:hack_categories).where(hack_categories: { category_id: financial_category_ids }).distinct
-  end
-
-  def self.with_knowledge_ids(*ids)
-    knowledge_category_ids = Category.knowledge_categories.where(id: ids).select(:id)
-    joins(:hack_categories).where(hack_categories: { category_id: knowledge_category_ids }).distinct
-  end
-
-  def self.with_scale_ids(*ids)
-    scale_category_ids = Category.scale_categories.where(id: ids).select(:id)
-    joins(:hack_categories).where(hack_categories: { category_id: scale_category_ids }).distinct
-  end
-
-  def self.ransackable_scopes(auth_object = nil)
-    %i[ with_popularity_ids with_financial_topic_ids with_audience_ids with_horizon_ids with_risk_ids with_implementation_ids
-        with_financial_ids with_knowledge_ids with_scale_ids ]
-  end
 end
